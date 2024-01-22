b59222ad0eb7ace5970412d8babfb2034ffe7cac
IN_MOURITECH\pratikshab <pratikshab.in@mouritech.com>
2024-01-22 12:18:52 +0530

    learining

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
+
+// alert("Hello, World!");
+let name = prompt("What is your name?");
+console.log("Hello, " + name + "!");
